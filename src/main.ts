44537ce40f7fed0393179a940066cab3678e99bb
Mauricio Rivera <mauriver21@gmail.com>
2023-04-23 13:38:07 -0500

    submit del form persona

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = /*html */ `
       </div>
       <div class="row">
         <div class="col-12">
-          <form class="row gy-3">
+          <form class="row gy-3" id="formulario-cliente">
             <div class="col-md-6">
               <label for="nombre" class="form-label">Nombre</label>
               <input name="nombre" id="nombre" class="form-control" />
@@ -45,6 +45,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = /*html */ `
             <div class="col-md-6">
               <label for="edad" class="form-label">Edad</label>
               <input name="edad" id="edad" class="form-control" />
+              <div class="form-text">Solo mayores de edad.</div>
             </div>
             <div class="col-md-6">
               <button class="btn btn-primary">Crear</button>
@@ -56,8 +57,39 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = /*html */ `
   </main>
 `;
 
-/**
- *       T
- * S            E
- *       B
- */
+const nombre = document.querySelector<HTMLInputElement>('[name="nombre"]')!;
+const apellido = document.querySelector<HTMLInputElement>('[name="apellido"]')!;
+const email = document.querySelector<HTMLInputElement>('[name="email"]')!;
+const edad = document.querySelector<HTMLInputElement>('[name="edad"]')!;
+const formularioCliente = document.getElementById(
+  "formulario-cliente"
+)! as HTMLFormElement;
+
+nombre.addEventListener("input", (event) => {
+  persona.nombre = (event.target as HTMLInputElement).value;
+});
+apellido.addEventListener("input", (event) => {
+  persona.apellido = (event.target as HTMLInputElement).value;
+});
+email.addEventListener("input", (event) => {
+  persona.email = (event.target as HTMLInputElement).value;
+});
+edad.addEventListener("input", (event) => {
+  persona.edad = Number((event.target as HTMLInputElement).value);
+});
+
+function formularioEsValido() {
+  if (persona.nombre === "") return false;
+  if (persona.apellido === "") return false;
+  if (persona.email === "") return false;
+  if (persona.edad <= 17) return false;
+  return true;
+}
+
+formularioCliente.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const esValido = formularioEsValido();
+  if (esValido) {
+    console.log(persona);
+  }
+});
