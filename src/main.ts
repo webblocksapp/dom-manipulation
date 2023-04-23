60bf334010819c189e84d9e0915db6de3e80a745
Mauricio Rivera <mauriver21@gmail.com>
2023-04-23 12:53:04 -0500

    Initial project setup

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import "./main.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = /*html */ `
+  <main>
+    <header>
+      <nav class="navbar bg-dark p-3">
+        <p class="text-light mb-0">Mi Aplicación</p>
+      </nav>
+    </header>
+    <div class="p-3">
+      Contenido
+    </div>
+  </main>
+`;
+
+/**
+ *       T
+ * S            E
+ *       B
+ */
